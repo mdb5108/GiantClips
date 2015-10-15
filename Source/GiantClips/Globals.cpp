5a75022d4a9252af61a2613944914573af89a79a
Minovsky <chnckun@gmail.com>
2015-10-15 10:10:13 -0600

    Added missing Pattern checking files.

---
+++ Source/GiantClips/Globals.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GiantClips.h"
+#include "Globals.h"
+
+AActor* UGlobals::activeHeadOfHair = NULL;
+
+int32 UGlobals::numberOfDiffHairs = 0;
